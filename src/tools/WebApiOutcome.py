87517d432b5ae2f7e06107b7a67f3ef4fc48527c
ignotochi <lorenzo@fetchtec.com>
2022-11-10 13:00:24 +0100

    T

--- src/tools/WebApiOutcome.py
+++ src/tools/WebApiOutcome.py
@@ -1,12 +1,16 @@
 import json
 from ..abstractions.IWebApiOutcome import IWebApiOutcome
 from ..abstractions.OutcomeJsonEncoder import OutcomeJsonEncoder
+from typing import TypeVar, Generic, List, Tuple
 
+T = TypeVar("T")
 
 class WebApiOutcome(IWebApiOutcome):
+    
+    result = [T]
 
-    def __init__(self, item = None):
-        if(item != None):
+    def __init__(self, item = [T]) -> T:
+        if (item != []):
          self.result = self.ToJson(item)
 
     def ToJson(self, item):
